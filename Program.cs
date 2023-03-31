0f216e80bb1cc612290fbcb30886ed0768d16383
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-03-31 22:40:37 +0700

    SystemTime

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SystemTime;
+
+class ProgramManager {
+static void Main(string[] args) {
+
+DateTime localDate = DateTime.Now;
+System.Console.WriteLine("Datetime Now is :" + localDate);
+
+}
+
+
+}
